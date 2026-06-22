f4119a1ae17a5d7889a11274c8503eae12707676
agalliski-netizen <agalliski@gmail.com>
2026-06-22 17:58:58 -0300

    Add PostHog analytics provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,21 @@
 import { Analytics } from '@vercel/analytics/next';
+import { useEffect } from 'react';
+import posthog from 'posthog-js';
+import { PostHogProvider } from 'posthog-js/react';
 
-function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }) {
+    useEffect(() => {
+          posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+                  api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://eu.i.posthog.com',
+                  person_profiles: 'identified_only',
+                  capture_pageview: true,
+          });
+    }, []);
+
   return (
-    <>
-    <Component {...pageProps} />
-  <Analytics />
-    </>
-  );
+        <PostHogProvider client={posthog}>
+          <Component {...pageProps} />
+        <Analytics />
+    </PostHogProvider>
+    );
 }
-
-export default MyApp;
