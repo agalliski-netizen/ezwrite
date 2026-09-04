ebf183d14d07e86b3be58b3437b1c154df031a02
ezwrite-app <ezwrite.app@gmail.com>
2026-09-04 17:26:22 -0300

    Create _document.js

---
+++ pages/_document.js
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+    return (
+          <Html>
+            <Head>
+              <meta name="google-site-verification" content="G_n0ZxvwGFpz1YsM9kfjmHlmtdGv1ZQy9Wn1S0lawFU" />
+      </Head>
+        <body>
+              <Main />
+              <NextScript />
+      </body>
+      </Html>
+    );
+}
